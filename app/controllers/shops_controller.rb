e27545be1b22075bf094cad7194187e22c13470e
Louis Angelini <louisangelini@gmail.com>
2014-05-29 02:38:17 +0100

    Add equation to fix urls. Creat shops to do

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -10,4 +10,8 @@ class ShopsController < ApplicationController
 	def show
 		@shop = Shop.find_by_id(params[:id])
 	end
+
+	def create
+		#todo
+	end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,11 @@
 class Shop < ActiveRecord::Base
   attr_accessible :address, :facebook, :lat, :lat, :logo, :lon, :name, :twitter, :url
+
+  before_save :validate_url
+
+  def validate_url
+  	shop = Shop.find(self.id)
+  	puts "id#{shop.id}"
+  	/^http/.match(shop.url) ? shop.url : "http://#{url}"
+  end
 end
