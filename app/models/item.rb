aff4bb5b725a7dcaf8960fef08dcb2ed7e8ae312
Loubot <louisangelini@gmail.com>
2014-06-02 20:38:38 +0100

    End of this branch

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,8 +15,7 @@
 
 class Item < ActiveRecord::Base
   #attr_accessible :available, :description, :name, :price, :shop_id, :size
-  has_attached_file :image1, :styles => { :medium => "300x300", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :image1, :content_type => /\Aimage\/.*\Z/
+ 
   
   belongs_to :shop, dependent: :destroy
 end
