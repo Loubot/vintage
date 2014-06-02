6bac95b0f6e1a537db94c12d850634549d7a6300
Loubot <louisangelini@gmail.com>
2014-06-02 14:07:17 +0100

    Implement file upload

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,30 +1,31 @@
 class ItemsController < ApplicationController
+	before_action :size_range, only: [:new]
 	
 	def new
 		@item = Item.new
-		@sizes = size_range
+		
 	end
 
 	def create
 		@item = Item.create(item_params)
-		flash[:success] = item_params
-		redirect_to :back
-		# if @item.save
-		# 	flash[:success] = "New item successfully saved"
-		# 	render 'new'
-		# else
-		# 	flash[:danger] = "Could not save item #{@item.errors.full_messages}"
-		# 	redirect_to :back
-		# end
+		# flash[:success] = item_params
+		# redirect_to :back
+		if @item.save
+			flash[:success] = "New item successfully saved"
+			render 'new'
+		else
+			flash[:danger] = "Could not save item #{@item.errors.full_messages}"
+			redirect_to :back
+		end
 	end
 
 
 	def size_range
-		(4..32).step(0.5).to_a
+		@sizes = (4..32).step(0.5).to_a
 	end	
 
 	private
 		def item_params
-			params.require(:item).permit(:available, :description, :name, :price, :shop_id, :size)
+			params.require(:item).permit(:available, :description, :name, :price, :shop_id, :size, :image1)
 		end
 end
