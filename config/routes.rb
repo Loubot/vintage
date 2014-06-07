fd6536692104881a5ab5cf6db5d8b0eef65ea62b
Loubot <louisangelini@gmail.com>
2014-06-07 14:08:23 +0100

    Move progress bar to end of item update form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Vintage::Application.routes.draw do
-  resources :merchants
-  resources :shops
+  resources :merchants do
+  	resources :shops
+  end
+  resources :shops do
+  	resources :items
+  end
   resources :items do
   	resources :photos
   end
